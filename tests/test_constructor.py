7e87b0a357488938117fe8cfd16ca88aeec25fa3
alex199508089-coder <alex199508089@gmail.com>
2026-08-15 11:19:11 +0400

    New commit8

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -7,13 +7,18 @@ from conftest import BASE_URL
 class TestConstructor:
 
     def test_buns_section_active_after_click(self, driver):
-        """Проверка активации раздела «Булки» после клика."""
+        """Проверка активации раздела «Булки» после клика (с переключением с «Соусов»)."""
         driver.get(BASE_URL)
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TAB_BUNS))
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(TAB_SAUCES))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(TAB_SAUCES)).click()
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.XPATH, "//div[contains(@class,'tab_tab_type_current')]//span[text()='Соусы']")
+            )
+        )
 
-
-        bun_tab = driver.find_element(*TAB_BUNS)
-        driver.execute_script("arguments[0].click();", bun_tab)
+        # Теперь кликаем по «Булки» обычным способом
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(TAB_BUNS)).click()
 
         active_tab = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located(
